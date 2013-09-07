9d25687b42343a695a913e853cc7a564fff0680e
ganc <yinzikat@thomas.bha.local>
2013-09-07 16:19:20 -0400

    Time Updated

--- src/data/Time.java
+++ src/data/Time.java
@@ -1,32 +1,88 @@
 package data;
 
+/**
+ * Stores hours and minute values
+ * @author ganc
+ *
+ */
 public class Time {
 	private int hour;
 	private int minute;
+	private static final int TIME_INTERVAL = 15;
 	
+	/** 
+	 * Empty constructor for Time
+	 */
 	public Time (){
-		
 	}
 	
+	
+	/**
+	 * Time constructor that stores hours and minutes
+	 * @param hours
+	 * @param minutes
+	 */
 	public Time (int hours, int minutes){
 		this.hour = hours;
-		this.minute = minutes;
+		this.minute = nearestInterval(minutes);
 	}
 	
+	/**
+	 * returns the level of precision of time (i.e. 15 minutes, everything is rounded to the nearest 15.) 
+	 * @return time interval
+	 */
+	public static int getInterval(){
+		return TIME_INTERVAL;
+	}
+	
+	/**
+	 *  returns stored hour value
+	 * @return stored hour value
+	 */
 	public int getHour(){
 		return hour;
 	}
 	
+	/**
+	 * returns stored minute value
+	 * @return stored minute value
+	 */
 	public int getMinute(){
 		return minute;
 	}
 	
+	/**
+	 * changes the stored hour value to parameter
+	 * @param hour the new hour value to be stored
+	 */
 	public void setHour(int hour){
 		this.hour = hour;
 	}
 	
+	/**
+	 * changes the stored minute value to parameter
+	 * @param minute the new minute value to be stored
+	 */
 	public void setMinute(int minute) {
-		this.minute = minute;
+		this.minute = nearestInterval(minute);
+	}
+	
+	/**
+	 * returns the time rounded to the nearest TIME_INTERVAL
+	 * @param time time to be rounded as
+	 * @return rounded time
+	 */
+	private int nearestInterval(int minute) {
+		if (minute % TIME_INTERVAL  == 0)
+			return minute;
+		else {
+			int delta = minute % TIME_INTERVAL;
+			if (delta > TIME_INTERVAL/2)
+				minute += (TIME_INTERVAL-delta);
+			 else 
+				minute -= delta;
+			return minute;
+		}
 	}
 	
 }
--- src/entities/Schedule.java
+++ src/entities/Schedule.java
@@ -1,15 +1,14 @@
 package entities;
 
-import java.sql.Time;
+import data.Time;
 
 
 public class Schedule {
 
 	private TimeEvent[][] timeArray = new TimeEvent[DAYS_A_WEEK][INTERVALS_A_DAY];
-	private static final int TIME_INTERVAL = 15;
 	private static final int DAYS_A_WEEK = 7;
 	private static final int HOURS_A_DAY = 24;
-	private static final int INTERVALS_A_DAY = (HOURS_A_DAY * 60) / (TIME_INTERVAL); 
+	private static final int INTERVALS_A_DAY = (HOURS_A_DAY * 60) / (Time.getInterval()); 
 	
 	public Schedule() {
 	}
@@ -20,24 +19,7 @@ public class Schedule {
 		
 		return true;
 	}
-	
-	/**
-	 * returns the time rounded to the nearest TIME_INTERVAL
-	 * @param time time to be rounded as
-	 * @return rounded time
-	 */
-	private int nearestInterval(Time t) {
-		if (time % TIME_INTERVAL  == 0)
-			return time;
-		else {
-			int delta = time % TIME_INTERVAL;
-			if (delta > TIME_INTERVAL/2)
-				time += (TIME_INTERVAL-delta);
-			 else 
-				time -= delta;
-			return time;
-		}
-	}
+
 	
 	public TimeEvent getEventAtTime(Time t){
 		return null;
