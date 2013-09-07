f938232a6d83ee4ca342da882c3847b5d58420da
unknown <yinzikat@thomas.bha.local>
2013-09-07 15:27:08 -0400

    changed entity interface to class

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,25 +1,28 @@
 package entities;
 
-public interface Entity {
+public class Entity {
 	
 	/*
 	 * Returns the schedule, in array form, of the Entity in question.
 	 */
-	public Schedule getSchedule();
+	public Schedule getSchedule(){
+		return null;}
 	
 	/*
 	 * Sets the schedule to "schedule" of the Entity in question.
 	 */
-	public boolean setSchedule(Object[] schedule);
+	public void setSchedule(Object[] schedule){}
 	
 	/*
 	 * Returns the entity which has an event at time t.
 	 */
-	public Entity eventAtTime(int t);
+	public Entity eventAtTime(int t){
+		return null;}
 	
 	/*
 	 * Returns the schedule, in array form, of the Entity in question ONLY including the available times.
 	 */
-	public Schedule timesFree();
+	public Schedule timesFree(){
+		return null;}
 	
 }
--- src/entities/Schedule.java
+++ src/entities/Schedule.java
@@ -3,17 +3,12 @@ package entities;
 public class Schedule {
 
 	private TimeEvent[][] timeArray = new TimeEvent[DAYS_A_WEEK][INTERVALS_A_DAY];
-	private String entityName;
 	private static final int TIME_INTERVAL = 15;
 	private static final int DAYS_A_WEEK = 7;
 	private static final int HOURS_A_DAY = 24;
 	private static final int INTERVALS_A_DAY = (HOURS_A_DAY * 60) / (TIME_INTERVAL); 
 	
 	public Schedule() {
-	}
-	
-	public Schedule(String entityName){
-		this.entityName = entityName;
 	}
 	
 	public boolean addEvent(String eventName, int startTime, int endTime) {
