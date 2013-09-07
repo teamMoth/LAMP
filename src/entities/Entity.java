99fa2089eeaf9ba06b0a90cf6176d0848fb29742
unknown <yinzikat@thomas.bha.local>
2013-09-07 15:40:24 -0400

    commit just in case

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,28 +1,44 @@
 package entities;
 
 public class Entity {
-	
-	/*
+
+	private String entityName;
+	private Schedule entitySchedule;
+
+	/**
 	 * Returns the schedule, in array form, of the Entity in question.
+	 * 
+	 * @return the entity's schedule
 	 */
-	public Schedule getSchedule(){
-		return null;}
-	
-	/*
+	public Schedule getSchedule() {
+		return null;
+	}
+
+	/**
 	 * Sets the schedule to "schedule" of the Entity in question.
+	 * 
+	 * @param schedule the schedule to be assigned to the entity
 	 */
-	public void setSchedule(Object[] schedule){}
-	
-	/*
-	 * Returns the entity which has an event at time t.
+	public void setSchedule(Object[] schedule) {
+	}
+
+	/**
+	 * Returns if entity has an event at time t.
+	 * 
+	 * @return if entity has an event at time t
 	 */
-	public Entity eventAtTime(int t){
-		return null;}
-	
-	/*
-	 * Returns the schedule, in array form, of the Entity in question ONLY including the available times.
+	public TimeEvent eventAtTime(int t) {
+		return null;
+	}
+
+	/**
+	 * Returns the schedule, in array form, of the Entity in question ONLY
+	 * including the available times.
+	 * 
+	 * @return
 	 */
-	public Schedule timesFree(){
-		return null;}
-	
+	public Schedule timesFree() {
+		return null;
+	}
+
 }
