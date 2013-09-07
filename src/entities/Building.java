ed9fd8f8cb2d4025842073ac0e1f12d36e69614a
TeamMoth2013 <teammoth2013@gmail.com>
2013-09-07 15:24:09 -0400

    tquinn Entites Added

---
+++ src/entities/Building.java
@@ -0,0 +1,35 @@
+package entities;
+
+public class Building implements Entity {
+
+	private Schedule buildingSchedule;
+	
+	public Building(Schedule buildingSchedule){
+		this.buildingSchedule = buildingSchedule;
+	}
+	
+	@Override
+	public Schedule getSchedule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setSchedule(Object[] schedule) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Entity eventAtTime(int t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Schedule timesFree() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/entities/Person.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Person implements Entity {
+
+	@Override
+	public Schedule getSchedule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setSchedule(Object[] schedule) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Entity eventAtTime(int t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Schedule timesFree() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
