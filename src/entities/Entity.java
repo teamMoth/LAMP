cbf294a93764bbb57e4130036f14c9f41184e640
TeamMoth2013 <teammoth2013@gmail.com>
2013-09-07 15:48:19 -0400

    tquinn added after removal

---
+++ src/entities/Entity.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class Entity {
+
+	private Schedule entitySchedule;
+
+	public Entity(Schedule entitySchedule){
+		this.entitySchedule = entitySchedule;
+	}
+
+	/*
+	 * Returns the schedule, in array form, of the Entity in question.
+	 */
+	public Schedule getSchedule(){
+		return entitySchedule;}
+
+	/*
+	 * Sets the schedule to "schedule" of the Entity in question.
+	 */
+	public void setSchedule(Schedule newSchedule){
+		this.entitySchedule = newSchedule;
+	}
+
+	/*
+	 * Returns the entity which has an event at time t.
+	 */
+	public TimeEvent eventAtTime(int t){
+		return entitySchedule.getEventAtTime(t);}
+
+	/*
+	 * Returns the schedule, in array form, of the Entity in question ONLY including the available times.
+	 */
+	public Schedule timesFree(){
+		return null;}
+
+}
