34d1375fcf8b39a9b5b0931a920bfdfa2a0d485b
Daniel Lins <freestate@freestate-Latitude-E6430.(none)>
2013-09-08 04:45:34 -0400

    new update qui-ness

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -9,36 +9,59 @@ import javax.swing.*;
 
 import model.Calendar;
 import model.TimeSlot;
+import data.Time;
 
 public class MainGUI extends JPanel{
 
 	private static final long serialVersionUID = 1L;
+	private static int PANEL_MIN_WIDTH = 100;
 	private static int CELL_HEIGHT = 30;
 	private static int CELL_WIDTH = 30;
 	private static int DAYS_IN_WEEK = 7;
-	private static int HOURS_IN_DAY = 18;
-	private static int SECTIONS_IN_HOUR = 2;
+	private static int HOURS_IN_DAY = 6;
+	private static int SECTIONS_IN_HOUR = 4;
 	private static boolean RAISED_CELL = true;
 	private Calendar myCalendar;
+	private int hardCodedNumber = 21;
+	private static Status status = Status.FREE;
 	
 	
 	public MainGUI(){
 		myCalendar = new Calendar(HOURS_IN_DAY * SECTIONS_IN_HOUR, DAYS_IN_WEEK);
 		
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		JLabel leftTitle = new JLabel("Left");
+		JLabel leftTitle = new JLabel("Time");
+		leftTitle.setPreferredSize(new Dimension(CELL_WIDTH, CELL_HEIGHT));
 		JLabel middleTitle = new JLabel("Middle");
 		JLabel rightTitle = new JLabel("Right");
 		
-		JTextArea textArea = new JTextArea("MON TUE WED THU FRI SAT SUN");
+		JTextArea textArea = new JTextArea("SUN MON TUE WED THU FRI SAT");
 		textArea.setFont(new Font("Serif", Font.ITALIC, 11));
 		textArea.setEditable(false);
 			    	
-		JPanel left = new JPanel();	
+		JPanel left = new JPanel();
+		left.setPreferredSize(new Dimension(PANEL_MIN_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT + hardCodedNumber));
+		left.setMinimumSize(new Dimension(PANEL_MIN_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT + hardCodedNumber));
+		left.setMaximumSize(new Dimension(PANEL_MIN_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT + hardCodedNumber));
+		left.setLayout(new BoxLayout(left, BoxLayout.Y_AXIS));
 		left.add(leftTitle);
+		for (int i = 0; i < HOURS_IN_DAY; i ++){
+			System.out.println(i);
+			for (int j = 0; j < 60 / Time.TIME_INTERVAL; j ++){
+				String time = (Time.START_HOUR + i) + ":" + (j * 15);
+				JLabel label = new JLabel(time);
+				label.setPreferredSize(new Dimension(PANEL_MIN_WIDTH, CELL_HEIGHT));
+				label.setMinimumSize(new Dimension(PANEL_MIN_WIDTH, CELL_HEIGHT));
+				label.setMaximumSize(new Dimension(PANEL_MIN_WIDTH, CELL_HEIGHT));
+				left.add(label);
+				System.out.println("label added");
+			}
+		}
 
 		JPanel middle = new JPanel();
-		middle.setPreferredSize(new Dimension(DAYS_IN_WEEK * CELL_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT));	
+		middle.setPreferredSize(new Dimension(DAYS_IN_WEEK * CELL_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT + hardCodedNumber));
+		middle.setMinimumSize(new Dimension(DAYS_IN_WEEK * CELL_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT + hardCodedNumber));
+		middle.setMaximumSize(new Dimension(DAYS_IN_WEEK * CELL_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT + hardCodedNumber));
 		middle.setLayout(new BoxLayout(middle, BoxLayout.Y_AXIS));
 		
 		JPanel midTop = new JPanel();
@@ -52,15 +75,87 @@ public class MainGUI extends JPanel{
 		middle.add(midBot);
 		
 		JPanel right = new JPanel();
-		right.add(rightTitle);
+		right.setPreferredSize(new Dimension(PANEL_MIN_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT + hardCodedNumber));
+		right.setMinimumSize(new Dimension(PANEL_MIN_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT + hardCodedNumber));
+		right.setMaximumSize(new Dimension(PANEL_MIN_WIDTH, HOURS_IN_DAY * SECTIONS_IN_HOUR * CELL_HEIGHT + hardCodedNumber));
+		right.setLayout(new BoxLayout(right, BoxLayout.Y_AXIS));
+		
+		JButton classButton = new JButton("CLASS");
+		classButton.setBackground(Color.RED);
+		classButton.setForeground(Color.YELLOW);
+		classButton.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                status = Status.CLASS;
+            }
+        });      
+		
+		JButton workButton = new JButton("WORK");
+		workButton.setBackground(Color.BLACK);
+		workButton.setForeground(Color.WHITE);
+		workButton.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                status = Status.WORK;
+            }
+        });      
+		
+		JButton busyButton = new JButton("BUSY");
+		busyButton.setBackground(Color.BLUE);
+		busyButton.setForeground(Color.WHITE);
+		busyButton.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                status = Status.BUSY;
+            }
+        });      
+		
+		JButton freeButton = new JButton("FREE");
+		freeButton.setBackground(Color.WHITE);
+		freeButton.setForeground(Color.BLACK);
+		freeButton.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                status = Status.FREE;
+            }
+        });      
+		
+		
+		right.add(classButton);
+		right.add(workButton);
+		right.add(busyButton);
+		right.add(freeButton);
 
-		addMouseListener(new MouseHandler());
+		addMouseListener(new MouseHandler(this));
 
 		add(left);
 		add(middle);
 		add(right);
+	
 	}
 	
+	private enum Status {
+		CLASS("CLASS", Color.RED),
+		WORK("WORK", Color.BLACK),
+		BUSY("BUSY", Color.BLUE),
+		FREE("FREE", Color.WHITE);
+		
+		private final String name;
+		private final Color color;
+		
+		Status(String name, Color color){
+			this.name = name;
+			this.color = color;
+		}
+	}
 	
 	private class CalendarGUI extends JComponent{
 
@@ -91,16 +186,34 @@ public class MainGUI extends JPanel{
 
 	
 	private class MouseHandler extends MouseAdapter {
+		MainGUI mainGUI;
+		
+		public MouseHandler(MainGUI mainGUI){
+			this.mainGUI = mainGUI;
+		}
+		
 		public void mouseReleased(MouseEvent event) {
+			
 			Point point = event.getPoint();
 			
-			System.out.println("point.x is " + point.x + "\npoint.y is " + point.y);
-
-			/* Determining the cell clicked */
-			int rowIndex = (int) (point.y / CELL_HEIGHT) - 1;
-			int colIndex = (int) (point.x / CELL_WIDTH) - 1;
+			System.out.println("GUI width: " + mainGUI.getWidth());
 			
-			System.out.println("x is " + colIndex + "\ny is " + rowIndex);
+			System.out.println("point.x: " + point.x + "\npoint.y: " + point.y);
+			
+			double xStart = (mainGUI.getWidth() / 2.0) - (DAYS_IN_WEEK * CELL_WIDTH / 2.0);
+			
+			System.out.println("xStart: " + xStart);
+			
+			if (point.x < xStart || point.y < hardCodedNumber)
+				return;
+			
+			double xDistance = point.x - xStart;
+			
+			System.out.println("xDistance: " + xDistance);
+			
+			/* Determining the cell clicked */
+			int rowIndex = (int) ((point.y - hardCodedNumber) / CELL_HEIGHT);
+			int colIndex = (int) (xDistance / CELL_WIDTH);
 			
 			if (rowIndex < HOURS_IN_DAY * SECTIONS_IN_HOUR && colIndex < DAYS_IN_WEEK){
 				processCell(rowIndex, colIndex);
@@ -110,7 +223,8 @@ public class MainGUI extends JPanel{
 	}
 	
 	public void processCell(int rowIndex, int colIndex){
-		myCalendar.schedule[rowIndex][colIndex].setColor(Color.RED);
+		myCalendar.schedule[rowIndex][colIndex].setColor(status.color);
+		myCalendar.schedule[rowIndex][colIndex].setName(status.name);
 	}
 	
 	private static TimeSlot[][] getGrid(Calendar calendar){
@@ -136,7 +250,7 @@ public class MainGUI extends JPanel{
 	    int upperLeftCornerX = (screenSize.width - frame.getWidth()) / 2;
 	    int upperLeftCornerY = (screenSize.height - frame.getHeight()) / 2;
 	    frame.setLocation(upperLeftCornerX, upperLeftCornerY);
-	   
+	    
 		frame.setVisible(true);
 	}
 	
--- src/model/TimeSlot.java
+++ src/model/TimeSlot.java
@@ -7,22 +7,23 @@ public class TimeSlot {
 	
 	private String name;
 	private Color color;
-	private JLabel label;
 	
 	public TimeSlot(){
 		this.name = "FREE";
 		this.color = Color.WHITE;
-		this.label = new JLabel(name);
 	}
 	
 	public TimeSlot(String name, Color color){
 		this.name = name;
 		this.color = color;
-		this.label = new JLabel(name);
 	}
 	
 	public void setColor(Color color){
 		this.color = color;
+	}
+	
+	public void setName(String name){
+		this.name = name;
 	}
 	
 	public Color getColor(){
