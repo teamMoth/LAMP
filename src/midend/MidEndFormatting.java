e4e36374f1413d07533807a8ff656236da5683e6
TeamMoth2013 <teammoth2013@gmail.com>
2013-09-07 23:31:45 -0400

    fixing what i messed up

--- src/midend/MidEndFormatting.java
+++ src/midend/MidEndFormatting.java
@@ -1,25 +1,56 @@
 package midend;
 
 import java.util.ArrayList;
-
+import comparison.scheduleComparison;
+import data.Time;
 import entities.Entity;
+import entities.EntityGroup;
 import entities.TimeEvent;
 
 public class MidEndFormatting {
 
-	static String freePeople(){
+	/**
+	 * returns a formatted String, which contains all the free people for a given time.
+	 * @param Entity Group eg
+	 * @param Time t
+	 * @return Schedule
+	 */
+	static String freePeople(EntityGroup eg, Time t){
 		String toReturn = "";
-
-
-
+		
+		scheduleComparison sc = new scheduleComparison(eg);
+		EntityGroup eg2 = sc.freeMembers(t);
+		
+		for (Entity e:eg2.getEntities()){
+			toReturn += e.getName() + " is free right now\n";
+		}
+		
 		return toReturn;
 	}
-
-	static String busyPeople(){
+	
+	/**
+	 * returns a formatted String, which contains all the free people for a given time.
+	 * @param Entity Group eg
+	 * @param Time t
+	 * @return Schedule
+	 */
+	static String busyPeople(EntityGroup eg, Time t){
 		String toReturn = "";
-
-
-
+		
+		scheduleComparison sc = new scheduleComparison(eg);
+		EntityGroup eg2 = sc.freeMembers(t);
+		EntityGroup eg3 = new EntityGroup();
+		
+		for (Entity e : eg.getEntities()){
+			if(!eg2.getEntities().contains(e)){
+				eg3.addEntity(e);
+			}
+		}
+		
+		for (Entity e : eg3.getEntities()){
+			toReturn += e.getName() + " is busy doing " + e.eventAtTime(t) + " and will be free at " + e.nextFree(t) + "\n";
+		}
+		
 		return toReturn;
 	}
 
