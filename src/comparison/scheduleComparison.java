80d1e50e9a6e3523961afbf23a53cae7ed65a42d
TeamMoth2013 <teammoth2013@gmail.com>
2013-09-07 21:30:36 -0400

    scheduleComparison tquinn

--- src/comparison/scheduleComparison.java
+++ src/comparison/scheduleComparison.java
@@ -9,17 +9,25 @@ import data.Time;
 import entities.Entity;
 import entities.EntityGroup;
 import entities.Schedule;
+import exceptions.InvalidEventException;
 
 public class scheduleComparison {
 
 	EntityGroup LOE; //List Of Entities
 	ArrayList<Entity> AOE;
 	
+	/**
+	 * scheduleComparison constructor that stores a list of entities
+	 * @param EntityGroup LOE
+	 */
 	public scheduleComparison(EntityGroup LOE){
 		this.LOE = LOE;
 		this.AOE = LOE.getEntities();
 	}
-	
+	/**
+	 * returns a Schedule, which contains all the free times for all members of the Entity Group LOE.
+	 * @return Schedule
+	 */
 	public Schedule freeTimes(){
 		
 		Schedule newSched = new Schedule();
@@ -42,4 +50,25 @@ public class scheduleComparison {
 		
 		return newSched;
 	}
+	
+	/**
+	 * returns a new EntityGroup, which contains all the free members for a given time range.
+	 * @param Time t
+	 * @return Schedule
+	 */
+	public EntityGroup freeMembers(Time t){
+		
+		EntityGroup freeMembs = new EntityGroup();
+		
+		Iterator<Entity> i = AOE.iterator();
+		while(i.hasNext()){
+			Entity nextEnt = i.next();
+			if(nextEnt.timesFree().contains(t)){
+				freeMembs.addEntity(nextEnt);
+			}
+		}
+		
+		return freeMembs;
+	}
+		
 }
